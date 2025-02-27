c890aa677b1c603e245c15c05cc2cd6bd2133491
Lseojung <sjung0314@gachon.ac.kr>
2025-02-28 07:45:42 +0900

    Feat : ADD marathon board

---
+++ app/src/main/java/androidtown/org/moveon/MarathonBoardFragment.kt
@@ -0,0 +1,4 @@
+package androidtown.org.moveon
+
+class MarathonBoardFragment {
+}
--- app/src/main/java/androidtown/org/moveon/RankingFragment.kt
+++ app/src/main/java/androidtown/org/moveon/RankingFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class RankingFragment : Fragment() {
 
@@ -21,20 +19,8 @@ class RankingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TabLayout과 ViewPager2 연결
-        val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
-        val viewPager = view.findViewById<ViewPager2>(R.id.viewPager)
-
-        // ViewPager2에 어댑터 설정
-        viewPager.adapter = RankingAdapter(this)
-
-        // TabLayoutMediator로 Tab과 ViewPager2 연결
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = when (position) {
-                0 -> "마라톤 랭킹" // 첫 번째 탭 이름
-                1 -> "주간 랭킹"   // 두 번째 탭 이름
-                else -> null
-            }
-        }.attach()
+        // "주간 랭킹" 텍스트 설정
+        val rankingTitle = view.findViewById<TextView>(R.id.tab_weekly_ranking)
+        rankingTitle.text = "주간 랭킹"
     }
 }
